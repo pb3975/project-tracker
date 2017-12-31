418a6159253098d67913466dfa3c09612efe59ec
pb3975 <metzwa.2@gmail.com>
2017-12-31 15:08:25 -0500

    new version running

--- project_tracker/__init__.py
+++ project_tracker/__init__.py
@@ -2,17 +2,20 @@ import os
 from flask import Flask
 from flask_mongoengine import MongoEngine
 
-from .views.views import projects
-from .views.admin import admin
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 db = MongoEngine(app)
 
-app.register_blueprint(projects)
-app.register_blueprint(admin)
 
+def register_blueprints(app):
+    from .views.views import projects
+    from .views.admin import admin
+    app.register_blueprint(projects)
+    app.register_blueprint(admin)
+
+register_blueprints(app)
 
 
 if __name__ == '__main__':
