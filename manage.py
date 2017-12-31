ec9ec94ce64a53b793c865db60777df5b6d9c1fd
pb3975 <metzwa.2@gmail.com>
2017-12-31 10:18:22 -0500

    Debugging

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from flask_script import Manager, Server
-from project_tracker import app
+from . import app
 
 manager = Manager(app)
 
