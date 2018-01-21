3a08adba44918834bbaee22acd7a4eddc0d5a98a
pb3975 <metzwa.2@gmail.com>
2018-01-21 11:41:31 -0500

    debugging heroku deployment file

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-sudo docker login -u $DOCKER_USER --password-stdin $DOCKER_PASS
+docker login -u $DOCKER_USER --password-stdin $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-sudo docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-sudo docker push $TRAVIS_REPO_SLUG
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker push $TRAVIS_REPO_SLUG
